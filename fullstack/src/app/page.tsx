d335aaa39c37ff319aadb4829ec6d45768f512eb
Josh <joshuaeduque@gmail.com>
2025-01-27 20:13:59 -0600

    Added routing to about page

--- fullstack/src/app/page.tsx
+++ fullstack/src/app/page.tsx
@@ -2,29 +2,37 @@
 'use client';
 
 import { useState } from 'react';
+import Link from 'next/link'
 
 export default function Home() {
 
+  type ResponseData = {
+    message: string
+  }
+
+  const [data, setData] = useState<ResponseData | null>(null);
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null);
 
   const fetchData = async () => {
     setLoading(true);
-
     fetch('/api/hello')
       .then(res => res.json())
-      .then(json => { setData(json) })
-      .catch(error => { setData({ message: 'An error occured calling the api route.' }) })
+      .then(json => setData({ message: json.message }))
+      .catch(reason => setData({ message: reason }))
       .finally(() => { setLoading(false) });
   }
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="h-fit flex flex-col items-center">
-        <button className='bg-blue-200 py-1 px-4 rounded' onClick={fetchData}>Click me</button>
-        {loading && <p>Loading...</p>}
-        {data && !loading && <p>{data.message}</p>}
+    <div className='p-4 flex flex-col gap-4'>
+      <div>
+        <Link href='/about' className='bg-blue-500 text-white px-2 py-1 rounded'>Go to about page</Link>
       </div>
+      <div>
+        <button className='bg-blue-500 text-white px-2 py-1 rounded' onClick={fetchData}>Fetch data</button>
+        {loading && <p>Loading data...</p>}
+        {data && !loading && <p>{JSON.stringify(data)}</p>}
+      </div>
+
     </div>
   );
 }
