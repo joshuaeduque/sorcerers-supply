e74d663176ae0d12a7eb976ab192a6a3f03bd603
JoesyP <157426260+JoesyP@users.noreply.github.com>
2025-01-31 09:45:46 -0600

    firebase config

---
+++ fullstack/src/app/firebase/config.ts
@@ -0,0 +1,17 @@
+import { FirebaseApp, FirebaseOptions, initializeApp } from 'firebase/app';
+import { Firestore, getFirestore } from 'firebase/firestore';
+
+const appOptions : FirebaseOptions = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MSG_SENDER_ID,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_API_MESURMENT_ID
+};
+
+const app : FirebaseApp = initializeApp(appOptions);
+const db : Firestore = getFirestore();
+
+export { app, db };
