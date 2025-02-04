9da31810e0044b374fc05ef9b652e18e0cbf8d8c
Josh <joshuaeduque@gmail.com>
2025-02-04 12:31:37 -0600

    Pass signup form data to parent

--- fullstack/src/app/signup/page.tsx
+++ fullstack/src/app/signup/page.tsx
@@ -2,12 +2,23 @@
 
 import { SignupForm } from '@/components/signup/signup-form'
 
+import { app } from '@/app/firebase/config';
+
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+import { FormEventHandler, useEffect } from 'react';
+
 export default function Signup() {
-    return (
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-          <div className="w-full max-w-sm">
-            <SignupForm className='dark' />
-          </div>
-        </div>
-      )
+
+  useEffect(() => {
+    const auth = getAuth(app);
+  }, []);
+
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm onSignupSubmit={(data)=>{console.log(data)}} className='dark' />
+      </div>
+    </div>
+  )
 }
--- fullstack/src/components/signup/signup-form.tsx
+++ fullstack/src/components/signup/signup-form.tsx
@@ -1,5 +1,10 @@
 import { cn } from "@/lib/utils"
+
+import React, { useState } from "react"
+
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import {
     Card,
     CardContent,
@@ -7,12 +12,34 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-export function SignupForm({
-    className,
-    ...props
-}: React.ComponentPropsWithoutRef<"div">) {
+
+// I think this is the right way to handle this in TypeScript?
+interface SignupData {
+    name: string,
+    email: string,
+    password: string
+}
+
+interface SignupFormProps extends React.ComponentPropsWithoutRef<'div'> {
+    onSignupSubmit: (data: SignupData) => void
+}
+
+export function SignupForm({ className, onSignupSubmit, ...props }: SignupFormProps) {
+
+    const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+
+        const data : SignupData = {
+            name: formData.get('name') as string,
+            email: formData.get('email') as string,
+            password: formData.get('password') as string
+        };
+
+        onSignupSubmit(data);
+    };
+
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
             <Card>
@@ -23,40 +50,41 @@ export function SignupForm({
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="flex flex-col gap-6">
                             <div className="grid gap-2">
                                 <Label htmlFor="name">Name</Label>
                                 <Input
+                                    name="name"
                                     id="name"
-                                    type=""
+                                    type="text"
                                     placeholder="John Smith"
-                                    required/>
+                                    required />
                             </div>
                             <div className="grid gap-2">
                                 <Label htmlFor="email">Email</Label>
                                 <Input
+                                    name="email"
                                     id="email"
                                     type="email"
                                     placeholder="m@example.com"
-                                    required
-                                />
+                                    required />
                             </div>
                             <div className="grid gap-2">
                                 <div className="flex items-center">
                                     <Label htmlFor="password">Password</Label>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <Input
+                                    name="password"
+                                    id="password"
+                                    type="password"
+                                    required />
                             </div>
-                            <Button type="submit" className="w-full">
-                                Sign up
-                            </Button>
+                            <Button type="submit" className="w-full">Sign up</Button>
                         </div>
                         <div className="mt-4 text-center text-sm">
                             Already have an account?{" "}
-                            <a href="/login" className="underline underline-offset-4">
-                                Login
-                            </a>
+                            <a href="/login" className="underline underline-offset-4">Login</a>
                         </div>
                     </form>
                 </CardContent>
