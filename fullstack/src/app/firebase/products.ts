63a34cbfc1fb07aea016af4a6b1f9ccdef4e2867
Chris <55931405+cbzoozoom7@users.noreply.github.com>
2025-02-21 10:10:16 -0600

    Update products.ts

--- fullstack/src/app/firebase/products.ts
+++ fullstack/src/app/firebase/products.ts
@@ -2,7 +2,7 @@ import { db } from '@/app/firebase/config'
 import { collection, DocumentData, getDocs, QueryDocumentSnapshot } from 'firebase/firestore'
 
 async function getProductDocuments(): Promise<QueryDocumentSnapshot<DocumentData, DocumentData>[]> {
-    const querySnapshot = await getDocs(collection(db, 'products'));
+    const querySnapshot = await getDocs(collection(db, 'creatures'));
     return querySnapshot.docs;
 };
 
