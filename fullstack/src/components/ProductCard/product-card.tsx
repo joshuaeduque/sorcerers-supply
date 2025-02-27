873cde4107955d5cd921cfa5f93b75dc8838e19d
JoesyP <157426260+JoesyP@users.noreply.github.com>
2025-02-27 17:09:59 -0600

    product card changes

--- fullstack/src/components/ProductCard/product-card.tsx
+++ fullstack/src/components/ProductCard/product-card.tsx
@@ -5,6 +5,7 @@ import {
     CardHeader,
 } from "@/components/ui/card"
 
+
 interface ProductCardProps {
     key?: string,
     name: string,
@@ -14,15 +15,16 @@ interface ProductCardProps {
 
 export function ProductCard({ name = 'Name', price = 0.00, imageSrc = '/next.svg' }: ProductCardProps) {
     return (
-        <Card className="w-48">
-            <CardHeader></CardHeader>
+        <Card className="md:w-48 w-28 group cursor-pointer shadow-white hover:shadow-white hover:shadow-md transition-all duration-300">
             <CardContent>
-                <img src={imageSrc} />
+                <div className="mt-4 overflow-hidden md:h-40 h-20 grid w-full place-items-center rounded-lg">
+                    <img src={imageSrc} className="h-full w-full object-fill group-hover:scale-110 transition-all duration-300"/>
+                </div>
             </CardContent>
             <CardFooter>
                 <div>
                     <p>{name}</p>
-                    <p className="text-gray-400">${price}</p>
+                    <p className="text-gray-400 text-sm">${price}</p>
                 </div>
             </CardFooter>
         </Card>
