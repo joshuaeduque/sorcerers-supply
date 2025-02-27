940d48de0ebb3add93cef146276a8979b34ae4e8
JoesyP <157426260+JoesyP@users.noreply.github.com>
2025-02-27 17:12:04 -0600

    page updates

--- fullstack/src/app/page.tsx
+++ fullstack/src/app/page.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 import { auth } from '@/app/firebase/config';
+import { getAuth, signOut } from "firebase/auth";
 import { getProductDocuments } from '@/app/firebase/products';
 
 import { ProductDocumentData } from "@/types/product-document-data";
@@ -19,11 +20,17 @@ import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
 
 import { useEffect, useState } from "react";
 
+import { useToast } from '@/hooks/use-toast';
+
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
 
   const [authenticated, setAuthenticated] = useState<boolean | null>(null);
   const [loadingProducts, setLoadingProducts] = useState(false);
   const [productDocuments, setProductDocuments] = useState<QueryDocumentSnapshot<DocumentData, DocumentData>[]>([]);
+  const { toast } = useToast();
+  const router = useRouter(); 
 
   useEffect(() => {
     // Get Firebase authentication
@@ -38,6 +45,7 @@ export default function Home() {
 
     // Get products from Firestore
     setLoadingProducts(true);
+
     getProductDocuments()
       .then(docs => {
         setProductDocuments(docs);
@@ -47,9 +55,36 @@ export default function Home() {
     return () => { unsubscribe() }
   }, []);
 
+  const handleClick = async () => {
+    const auth = getAuth();
+
+    if (!authenticated) {
+      router.push('/login');
+      return;
+    }
+    else {
+      try {
+        await signOut(auth);
+        toast({
+          title: "Signed out",
+          variant: "success",
+          description: "You have been signed out.",
+        });
+        router.push('/');
+      } catch (error) {
+        console.error(error);
+        toast({
+          title: "Sign out failed",
+          variant: "destructive",
+          description: "Please try again.",
+        });
+      }
+    }
+  };
+
   return (
     <div>
-      <SiteHeader authenticated={authenticated} onAuthClicked={() => { console.log('auth button clicked') }} />
+      <SiteHeader authenticated={authenticated} onAuthClicked={handleClick} />
       <div className="px-4 py-1 border-b border-gray-800 flex justify-end">
         <DropdownMenu >
           <DropdownMenuTrigger>Sort by</DropdownMenuTrigger>
@@ -62,7 +97,7 @@ export default function Home() {
       <div className="px-4 py-2">
         <p>Products</p>
       </div>
-      <div className="px-4 flex flex-wrap gap-4">
+      <div className="p-[2rem] grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 place-items-center">
         {loadingProducts ? <LoadingSpinner/> : productDocuments.map((doc) => {
           const data = doc.data() as QueryDocumentSnapshot<ProductDocumentData>;
           return (
